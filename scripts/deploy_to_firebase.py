948b0e715b1c28117619832541380b76465516c8
Smarajit Das <sdas9696969@gmail.com>
2023-06-20 00:02:10 +0530

    Update

--- scripts/deploy_to_firebase.py
+++ scripts/deploy_to_firebase.py
@@ -2,21 +2,31 @@ import requests
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+headers = {"accept": "application/json"}
 
-# Fetch data from API
-response = requests.get('https://corona.lmao.ninja/v3/covid-19/historical/india?lastdays=all')
-data = response.json()
-print(data);
-
-# Initialize Firebase
 cred = credentials.Certificate('covid19_tracker_private_key.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Upload data to Firebase
-collection_ref = db.collection('data')
-for item in data:
-    doc_ref = collection_ref.document()
-    doc_ref.set(item)
+def getCountriesList():
+    countriesListResponse = requests.get('https://disease.sh/v3/covid-19/gov/', headers=headers)
+    if (countriesListResponse.status_code != 200):
+        return getCountriesList()
+    return countriesListResponse.json()
+
+countriesList = getCountriesList()
+
+def getHistoricalDataForCountry(country):
+    countryResponse = requests.get('https://corona.lmao.ninja/v3/covid-19/historical/'+ country + '?lastdays=all', headers=headers)
+    if (countryResponse.status_code != 200):
+        return getHistoricalDataForCountry(country)
+    return countryResponse.json()
+
+for country in countriesList:
+    historicalData = getHistoricalDataForCountry(country)
+    collection_ref = db.collection('historicalData')
+    doc_ref = collection_ref.document(country)
+    doc_ref.set(historicalData)
+
 
 print('Data uploaded to Firebase successfully!')
